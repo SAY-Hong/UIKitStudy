4b40b626d95815fca645d52f16c82ab11483f32d
SAY-Hong <bobseahee@gmail.com>
2023-10-30 22:31:29 +0900

    UIKit 첫 시작

---
+++ ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  UIKitStudy
+//
+//  Created by 홍세희 on 2023/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func button(_ sender: Any) {
+        
+    }
+    
+}
+
